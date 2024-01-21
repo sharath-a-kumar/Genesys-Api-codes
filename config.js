4c234d3a62ff98cf088011b28194625d515e3d2c
Sharath Kumar A <sharathkumar3113@gmail.com>
2024-01-21 23:20:45 +0530

    config

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+module.exports = {
+    database: {
+      user: 'postgres',
+      host: 'localhost',
+      database: 'postgres',
+      password: 'sky@123',
+      port: '5432',
+    },
+  };
+  
