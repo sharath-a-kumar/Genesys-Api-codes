a9ea0f41bc9d1c0c276bb2125b9d06e2b7a587de
Sharath Kumar A <118186063+Shar3113@users.noreply.github.com>
2024-01-28 13:01:36 +0530

    Config file

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
-// config.js
 module.exports = {
-    database: {
-      user: 'postgres',
-      host: 'localhost',
-      database: 'postgres',
-      password: 'sky@123',
-      port: '5432',
-    },
-  };
-  
+  clientId: 'c3129afe-43af-4b5e-818f-7e5c0fdbbb6e',
+  clientSecret: 'F9xPK0fdjTYn0btfHazbVw8rd5GGLwkBpU5BcMgVRZg',
+  database: {
+    
+    host: 'localhost',
+    user: 'postgres',
+    password: 'sky@123',
+    database: 'conversationdb',
+    port: '5432',
+  },
+};
